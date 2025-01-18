6e862a33c03ec61bcf28d2dbfded20d591b090a2
Kevin Lefebvre <kevin@gbamerica.net>
2025-01-17 19:24:35 -0500

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Kevin Lefebvre on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
